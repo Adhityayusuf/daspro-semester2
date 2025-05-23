b1d51e2cd35b89fcf07df874b71378e733ca38cc
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2025-05-23 22:18:48 +0700

    p

---
+++ pertemuan 12/Mahasiswa13.java
@@ -0,0 +1,20 @@
+public class Mahasiswa13 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    public Mahasiswa13() {
+    }
+
+    public Mahasiswa13(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+
+    public void tampilInfformasi() {
+        
+    }
+}
